0ec44ff021f9fabdd6eaaed9d06344667189528e
sam <samyoung17a@gmail.com>
2018-02-05 17:56:02 +0000

    Add itemised list

--- supermarket.py
+++ supermarket.py
@@ -1,11 +1,27 @@
+# coding=utf-8
+
+
 class Item:
 
-    def __init__(self, name, unit_price, quantity, categories=[]):
+    def __init__(self, name, unit_price, quantity, unit=None, categories=[]):
         self.name = name
         self.unit_price = unit_price
         self.quantity = quantity
+        self.unit = unit
         self.categories = categories
 
 
 def price_items(items):
     return sum(map(lambda item: item.unit_price * item.quantity, items))
+
+
+def format_item(item):
+    if item.unit is None:
+        return '{}\t{}:\t\t\t£{:3.2f}'.format(item.quantity, item.name, item.unit_price * item.quantity)
+    else:
+        return '\t{}:\t{:3.3f}{} @ £{:3.2f}/{}\t£{:3.2f}'\
+            .format(item.name, item.quantity, item.unit, item.unit_price, item.unit, item.unit_price * item.quantity)
+
+
+def itemised_receipt(items):
+    return '\n'.join(map(format_item, items)) + '\n'
--- supermarkettest.py
+++ supermarkettest.py
@@ -1,13 +1,44 @@
+# coding=utf-8
+
 import unittest
 from supermarket import *
 
 
 class TestBasicPricing(unittest.TestCase):
 
-    def test_price_single_item(self):
-        items = [Item('onion', 1, 3.2)]
+    def test_price_one_item(self):
+        items = [Item('onion', 0.75, 0.8)]
         price = price_items(items)
-        self.assertEquals(price, 3.2)
+        self.assertAlmostEquals(price, 0.6)
+
+    def test_price_two_items(self):
+        items = [Item('onion', 0.75, 0.8), Item('strawberries', 1.2, 1)]
+        price = price_items(items)
+        self.assertAlmostEquals(price, 1.8)
+
+
+class TestItemisedReceipt(unittest.TestCase):
+
+    def test_itemise_one_item(self):
+        items = [Item('ball', 1.0, 1)]
+        receipt = itemised_receipt(items)
+        self.assertEquals(receipt, '1\tball:\t\t\t£1.00\n')
+
+    def test_itemise_two_items(self):
+        items = [Item('ball', 1.0, 1), Item('pen', 12.0, 2)]
+        receipt = itemised_receipt(items)
+        self.assertEquals(receipt, '1\tball:\t\t\t£1.00\n2\tpen:\t\t\t£24.00\n')
+
+    def test_itemise_onions(self):
+        items = [Item('onions', 0.75, 0.8, unit='kg')]
+        receipt = itemised_receipt(items)
+        self.assertEquals(receipt, '\tonions:\t0.800kg @ £0.75/kg\t£0.60\n')
+
+    def test_mixed_list(self):
+        items = [Item('ball', 1.0, 1), Item('pen', 12.0, 2), Item('onions', 0.75, 0.8, unit='kg')]
+        receipt = itemised_receipt(items)
+        self.assertEquals(receipt, '1\tball:\t\t\t£1.00\n2\tpen:\t\t\t£24.00\n\tonions:\t0.800kg @ £0.75/kg\t£0.60\n')
+
 
 if __name__ == '__main__':
     unittest.main()
